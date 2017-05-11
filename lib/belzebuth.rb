a5c8375593bd336c5b6a4b36a0a3b0cf17d44c5a
Pier-Hugues Pellerin <phpellerin@gmail.com>
2017-05-11 13:34:45 -0400

    make it easier to use

--- lib/belzebuth.rb
+++ lib/belzebuth.rb
@@ -1,4 +1,8 @@
+# encoding: utf-8
 require "belzebuth/version"
+require "belzebuth/wait_condition"
+require "belzebuth/response"
+require "belzebuth/process"
 
 module Belzebuth
   # Your code goes here...
